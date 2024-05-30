da1e031198050f036ca4e66958198ed86e8627cf
armin <arminyahyaa@gmail.com>
2024-05-31 00:55:11 +0330

    Change nextj configuration file

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	basePath: "/cv-nextjs",
+	output: "export",  // <=== enables static exports
+
+};
 
 export default nextConfig;
