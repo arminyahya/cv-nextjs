a3e8d3352e235bdb3258eb6328c2abea7eebcf30
arminyahya <arminyahyaa@gmail.com>
2024-11-30 16:05:17 +0330

    chore: change style selectors of footer and header

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -33,13 +33,13 @@ export default function Header({ }: HeaderProps) {
 	}
 
 	return (
-		<div className="flex justify-end items-center mb-0.5">
+		<header className="flex justify-end items-center mb-0.5">
 			<div className="header_item inline-block mx-1" onClick={switchTheme}>
 				{currentTheme === 'light' ? <DarkModeIcon /> : <LightModeIcon />}
 			</div>
 			<div className="header_item inline-block mx-1" onClick={switchLang} >
 				<MultiLangIcon />
 			</div>
-		</div>
+		</header>
 	)
 }
