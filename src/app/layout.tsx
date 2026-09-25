b5833c9c94afa35fb8e1b3bb7c1fe4f5367c1727
arminyahya <arminyahyaa@gmail.com>
2026-09-25 11:53:46 +0330

    chore: remove dark mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,6 @@
 "use client";
 import localFont from "next/font/local";
 
-import "./globals.css";
-import Header from "@/components/header";
-import AppContext from "@/appContext";
-import Footer from "@/components/footer";
 import { useParams } from "next/navigation";
 
 const openSans = localFont({ src: "../../public/OpenSans.ttf" });
@@ -22,7 +18,6 @@ export default function RootLayout({
           ( openSans.className) + ' relative my-0 mx-auto w-full bg-background-color max-w-[1000px]'
         }
       >
-        <Header />
         {children}
         <div id="portal-modal"></div>
       </body>
